6603cae911e12b72e529065b34c771863beb9397
Ranausama68 <145536360+Ranausama68@users.noreply.github.com>
2024-02-19 12:17:26 +0500

    added

--- addagent.php
+++ addagent.php
@@ -6,13 +6,43 @@ require_once "./db.php";
 
 
 
-$firstname = $lastname = $email = $password  = null;
-$error = $success = null;
+$firstname = $lastname = $email = $password = $role = null;
+$success = $error = null;
 
-
-
-// Form Handler 
+ 
 if($_SERVER['REQUEST_METHOD'] === "POST"){
+  if(empty($_POST['firstname'])){
+    $error = "Firstname is required!";
+}
+
+if(empty($_POST['lastname'])){
+    $error = "Lastname is required!";
+}
+
+if(empty($_POST['email'])){
+    $error = "Email is required!";
+}
+else {
+    $email = cleanData($_POST['email']);
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $error = "Email is incorrect!";
+    }
+    $query = "SELECT id FROM users WHERE email = '{$email}'";
+    $result = $con->query($query);
+    if($result->num_rows > 0){
+        $error = "Email already exists!";
+    }
+}
+if(empty($_POST['role'])){
+  $error = "Choose your Role!";
+}
+if(empty($_POST['pwd'])){
+    $error = "Password is required!";
+}
+
+ else if (empty($error)) {
+ 
+
     $firstname = cleanData($_POST['firstname']);
     $lastname = cleanData($_POST['lastname']);
     $email = cleanData($_POST['email']);
@@ -31,6 +61,9 @@ if($_SERVER['REQUEST_METHOD'] === "POST"){
         $success = "User Added Successfully...";
     }
 }
+}
+
+
 
 
 ?>
@@ -49,31 +82,37 @@ if($_SERVER['REQUEST_METHOD'] === "POST"){
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
-    <title>Form Design</title>
+    <title>Add Users</title>
   </head>
   <body>
    </div>
    
    <section class="container my-2 w-50 text-dark p-2">
-    <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST"  class="row g-3 p-3">
-    <h1 class="text-center">ADD</h1>
+    <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST"  class="row g-3 p-3" novalidate>
+    <h1 class="text-center">ADD Users</h1>
     <?php
-        if(isset($success)){
-    ?>
-        <div class="alert alert-success">
-            <?= $success ?>
-        </div>
-    <?php
-        }
-    ?>
+            if(isset($success)){
+                
+        ?>
+            <div class="alert alert-success"><?= $success ?></div>
+        <?php
+            }
+            if(isset($error)){
+        ?>
+            <div class="alert alert-danger">
+                <?= $error ?>
+            </div>
+        <?php
+            }
+        ?>
     
-        <div class="col-md-6">
+        <div class="col-md-6 ">
             <label for="validationDefault01" class="form-label">First name</label>
-            <input type="text" class="form-control" id="validationDefault01" name="firstname" required>
+            <input type="text" class="form-control" id="validationDefault01" name="firstname" >
           </div>
           <div class="col-md-6">
             <label for="validationDefault02" class="form-label">Last name</label>
-            <input type="text" class="form-control" id="validationDefault02" name="lastname" required>
+            <input type="text" class="form-control" id="validationDefault02" name="lastname" >
           </div>
         
         <div class="col-md-6">
@@ -85,8 +124,8 @@ if($_SERVER['REQUEST_METHOD'] === "POST"){
           <input type="password" class="form-control" id="inputPassword4"name="pwd">
         </div>
         <div class="col-md-4">
-          <label for="inputState" class="form-label">User type</label>
-          <select id="inputState" class="form-select" name="role" >
+          <label for="inputState" class="form-label" name="role">User Role</label>
+          <select id="inputState" class="form-select">
             <option selected>Choose...</option>
             <option>User</option>
             <option>Agent</option>
--- signup.php
+++ signup.php
@@ -1,3 +1,81 @@
+<?php
+session_start();
+
+require_once "./db.php";
+
+$firstname = $lastname = $email = $password = null;
+$success = $error = null;
+
+if($_SERVER['REQUEST_METHOD'] === "POST"){
+    if(empty($_POST['firstname'])){
+        $error = "Firstname Is Required";
+    }
+    if(empty($_POST['lastname'])){
+        $error = "Lastname Is Required";
+    }
+    if(empty($_POST['email'])){
+        $error = "Email Is Required";
+    }else{
+        $email = cleanData($_POST['email']);
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $error = "Email Is Incorrect";
+        }
+        $query = "SELECT id FROM users WHERE email = '$email'";
+        $result =$con->query($query);
+        if($result->num_rows > 0){
+            $error = "Email Already Exists!";
+        }
+    }
+    if(empty($_POST['pwd'])){
+        $error = "Password Is Required";
+    }
+    else if($_POST['pwd'] !== $_POST['cpwd']){
+        $error = "Passwords doesn't match!";
+    }
+    else if(empty($error)) {
+        $firstname = cleanData($_POST['firstname']);
+        $lastname = cleanData($_POST['lastname']);
+        $email = cleanData($_POST['email']);
+        $password = cleanData($_POST['pwd']);
+
+        $hashedpwd = md5($password);
+
+        $query = "INSERT INTO users (firstname,lastname,email,password) VALUES ('{$firstname}', '{$lastname}','{$email}','{$hashedpwd}')";
+
+        $result = $con->query($query);
+        if($result){
+            $success = "Account Created Successfully...";
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,35 +86,50 @@
       <div class="container">
         <div class="box form-box">
             <header>Sign Up</header>
-            <form >
+            <form action="<?= htmlspecialchars($_SERVER['PHP_SELF'])?>" method="POST" novalidate >
+            <?php
+            if(isset($success)){
+                
+        ?>
+            <div class="alert alert-success"><?= $success ?></div>
+        <?php
+            }
+            if(isset($error)){
+        ?>
+            <div class="alert alert-danger">
+                <?= $error ?>
+            </div>
+        <?php
+            }
+        ?>
                 <div class="field input">
                     <label for="username">First Name</label>
-                    <input type="text" name="firstname" id="username" autocomplete="off" required>
+                    <input type="text" name="firstname" id="username" autocomplete="off">
                 </div>
 
                 <div class="field input">
                     <label for="username">Last Name</label>
-                    <input type="text" name="lastname" id="username" autocomplete="off" required>
+                    <input type="text" name="lastname" id="username" autocomplete="off" >
                 </div>
 
                 <div class="field input">
                     <label for="email">Email</label>
-                    <input type="text" name="email" id="email" autocomplete="off" required>
+                    <input type="text" name="email" id="email" autocomplete="off">
                 </div>
 
                 <div class="field input">
                     <label for="password">Password</label>
-                    <input type="password" name="password" id="password" autocomplete="off" required>
+                    <input type="password" name="pwd" id="password" autocomplete="off">
                 </div>
 
                 <div class="field input">
                     <label for="password"> Confirm Password</label>
-                    <input type="password" name="cpassword" id="password" autocomplete="off" required>
+                    <input type="password" name="cpwd" id="password" autocomplete="off">
                 </div>
 
                 <div class="field">
                     
-                    <input type="submit" class="btn" name="submit" value="Register" required>
+                    <input type="submit" class="btn" name="submit" value="Register">
                 </div>
                 <div class="links">
                     Already a member? <a href="login.php">Sign In</a>
